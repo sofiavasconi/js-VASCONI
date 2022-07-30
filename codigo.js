d4f5d105189b1baa73862cc14109f3522554eb44
sofivasconi <sofivasconi@gmail.com>
2022-07-30 14:46:56 -0300

    clase 5 commit

--- codigo.js
+++ codigo.js
@@ -1,5 +1,21 @@
-let numero = parseInt(prompt("Ingresa un numero para repetir el mensaje"));
-for (let i=0; i<numero; i++) {
-    alert("Hola");
-    console.log("Hola");
+let ingresaNumero = parseInt(prompt("Ingresa el número de camisetas titulares que querés comprar"));
+function sumarNumeros (num1, num2) {
+        let suma=num1+num2;
+        alert ("El precio total de tu compra es: "+suma);
 }
+
+sumarNumeros (8000,ingresaNumero);
+
+
+
+
+function camisetas(titular, suplente) {
+    this.titular=titular;
+    this.suplete=suplente;
+}
+
+const titular1=new camiseta ("TITULAR $8.000");
+const suplente1=new camiseta ("SUPLENTE $6.000");
+
+alert (titular1);
+alert (suplente1);
