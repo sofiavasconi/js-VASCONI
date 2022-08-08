6216bcee77686e3e2449aa6d815987f6a8e2532f
sofivasconi <sofivasconi@gmail.com>
2022-08-08 18:23:54 -0300

    primera entrega

--- js/codigo.js
+++ js/codigo.js
@@ -1,74 +1,74 @@
 const camisetasNac = [
-    {categoria: "nacional", club: "boca", tipo: "titular", precio: 8000},
+    {categoria: "nacional", club: "boca", tipo: "titular", precio: 15000},
 
-    {categoria: "nacional", club: "boca", tipo: "suplente", precio: 6000},
+    {categoria: "nacional", club: "boca", tipo: "suplente", precio: 10000},
 
-    {categoria: "nacional", club: "river", tipo: "titular", precio: 8000},
+    {categoria: "nacional", club: "river", tipo: "titular", precio: 15000},
 
-    {categoria: "nacional", club: "river", tipo: "suplente", precio: 6000},
+    {categoria: "nacional", club: "river", tipo: "suplente", precio: 10000},
 
-    {categoria: "nacional", club: "racing", tipo: "titular", precio: 8000},
+    {categoria: "nacional", club: "racing", tipo: "titular", precio: 15000},
 
-    {categoria: "nacional", club: "racing", tipo: "suplente", precio: 6000},
+    {categoria: "nacional", club: "racing", tipo: "suplente", precio: 10000},
 
-    {categoria: "nacional", club: "independiente", tipo: "titular", precio: 8000},
+    {categoria: "nacional", club: "independiente", tipo: "titular", precio: 15000},
 
-    {categoria: "nacional", club: "independiente", tipo: "suplente", precio: 6000},
+    {categoria: "nacional", club: "independiente", tipo: "suplente", precio: 10000},
 
-    {categoria: "nacional", club: "san lorenzo", tipo: "titular", precio: 8000},
+    {categoria: "nacional", club: "san lorenzo", tipo: "titular", precio: 15000},
 
-    {categoria: "nacional", club: "san lorenzo", tipo: "suplente", precio: 6000},
+    {categoria: "nacional", club: "san lorenzo", tipo: "suplente", precio: 10000},
 
-    {categoria: "nacional", club: "velez", tipo: "titular", precio: 8000},
+    {categoria: "nacional", club: "velez", tipo: "titular", precio: 15000},
 
-    {categoria: "nacional", club: "velez", tipo: "suplente", precio: 6000},
+    {categoria: "nacional", club: "velez", tipo: "suplente", precio: 10000},
 
-    {categoria: "nacional", club: "huracan", tipo: "titular", precio: 8000},
+    {categoria: "nacional", club: "huracan", tipo: "titular", precio: 15000},
 
-    {categoria: "nacional", club: "huracan", tipo: "suplente", precio: 6000},
+    {categoria: "nacional", club: "huracan", tipo: "suplente", precio: 10000},
 ]
 
 const camisetasSel = [
-    {seleccion: "seleccion", pais: "argentina", precio: 8000},
+    {seleccion: "seleccion", pais: "argentina", precio: 15000},
 
-    {seleccion: "seleccion", pais: "brasil", precio: 8000},
+    {seleccion: "seleccion", pais: "brasil", precio: 15000},
 
-    {seleccion: "seleccion", pais: "uruguay", precio: 8000},
+    {seleccion: "seleccion", pais: "uruguay", precio: 15000},
 
-    {seleccion: "seleccion", pais: "italia", precio: 8000},
+    {seleccion: "seleccion", pais: "italia", precio: 15000},
 
-    {seleccion: "seleccion", pais: "francia", precio: 8000},
+    {seleccion: "seleccion", pais: "francia", precio: 15000},
 
-    {seleccion: "seleccion", pais: "alemania", precio: 8000},
+    {seleccion: "seleccion", pais: "alemania", precio: 15000},
 
-    {seleccion: "seleccion", pais: "inglaterra", precio: 8000},
+    {seleccion: "seleccion", pais: "inglaterra", precio: 15000},
 
-    {seleccion: "seleccion", pais: "españa", precio: 8000},
+    {seleccion: "seleccion", pais: "españa", precio: 15000},
 ]
 
 const camisetasInt = [
 
-    {categoria: "internacional", club: "psg", precio: 8000},
+    {categoria: "internacional", club: "psg", precio: 15000},
 
-    {categoria: "internacional", club: "barcelona", precio: 8000},
+    {categoria: "internacional", club: "barcelona", precio: 15000},
 
-    {categoria: "internacional", club: "real madrid", precio: 8000},
+    {categoria: "internacional", club: "real madrid", precio: 15000},
 
-    {categoria: "internacional", club: "juventus", precio: 8000},
+    {categoria: "internacional", club: "juventus", precio: 15000},
 
-    {categoria: "internacional", club: "inter", precio: 8000},
+    {categoria: "internacional", club: "inter", precio: 15000},
 
-    {categoria: "internacional", club: "manchester city", precio: 8000},
+    {categoria: "internacional", club: "manchester city", precio: 15000},
 
-    {categoria: "internacional", club: "manchester united", precio: 8000},
+    {categoria: "internacional", club: "manchester united", precio: 15000},
 
-    {categoria: "internacional", club: "liverpool", precio: 8000},
+    {categoria: "internacional", club: "liverpool", precio: 15000},
 
-    {categoria: "internacional", club: "bayern munich", precio: 8000},
+    {categoria: "internacional", club: "bayern munich", precio: 15000},
 
-    {categoria: "internacional", club: "flamengo", precio: 8000},
+    {categoria: "internacional", club: "flamengo", precio: 15000},
 
-    {categoria: "internacional", club: "palmeiras", precio: 8000},
+    {categoria: "internacional", club: "palmeiras", precio: 15000},
 
 ];
 
@@ -95,11 +95,11 @@ let cantidad = parseInt(prompt("Ingrese cuántas camisetas quiere comprar."));
 
 if (camiseta === 1) {
 
-    let resultado = 8000 * cantidad;
+    let resultado = 15000 * cantidad;
     alert ("El precio total de tu compra es: $"+resultado);
 }else if (camiseta === 2){
 
-        let resultado = 6000 * cantidad;
+        let resultado = 10000 * cantidad;
         alert ("El precio total de tu compra es: $"+resultado);
 }else{
     alert ("ERROR. Debe introducir 1 o 2.");
@@ -117,7 +117,7 @@ if (camiseta === 1) {
         let cantidad = parseInt(prompt("Ingrese cuántas camisetas quiere comprar."));
         {
         
-            let resultado = 8000 * cantidad;
+            let resultado = 15000 * cantidad;
             alert ("El precio total de tu compra es: $"+resultado);
         }
     }else{
@@ -134,7 +134,7 @@ if (camiseta === 1) {
             let cantidad = parseInt(prompt("Ingrese cuántas camisetas quiere comprar."));
             {
             
-                let resultado = 8000 * cantidad;
+                let resultado = 15000 * cantidad;
                 alert ("El precio total de tu compra es: $"+resultado);
             }
         }
