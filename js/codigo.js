a72599f1b063a470f20a3d5e9b2840a61b66783b
sofivasconi <sofivasconi@gmail.com>
2022-08-20 16:42:49 -0300

    segunda entrega

--- js/codigo.js
+++ js/codigo.js
@@ -147,20 +147,10 @@ function agregarAlCarrito (id){
 
 //actualizar el carrito
 function actualizarCarrito(){
-<<<<<<< HEAD
         agregarItemsAlCarrito ();
         mostrarTotal ();
 
-
-//guardar carrito en localstorage
-
-=======
-    agregarItemsAlCarrito ();
-    mostrarTotal ();
-
-
 //agregar a local storage
->>>>>>> eventos_rama
 localStorage.setItem("CARRITO", JSON.stringify(carrito));
 }
 
@@ -224,33 +214,18 @@ function cambiarNumeroDeUnidades(action, id){
 //total
 function mostrarTotal () {
     let total = 0;
-<<<<<<< HEAD
-        itemsTotales = 0;
-
-    carrito.forEach ((item) => {
-        total += item.precio * item.numeroDeUnidades;
-        itemsTotales += item.numeroDeUnidades;
-=======
     let unidades = 0;
 
     carrito.forEach ((item) => {
         total += item.precio * item.numeroDeUnidades;
         unidades += item.numeroDeUnidades;
-
->>>>>>> eventos_rama
     });
-
 
     precioTotal.innerHTML = `
         <div class="total-title">TOTAL: $${total}</div>
-<<<<<<< HEAD
     `;
-    notificacionCantidad.innerHTML = `${itemsTotales}`;
-=======
-    `
-    notificacionCantidad.innerHTML = unidades;
->>>>>>> eventos_rama
-};
+    notificacionCantidad.innerHTML = `${unidades}`;
+}
 
 
 //borrar productos del carrito
@@ -328,7 +303,4 @@ cerrarCarrito.onclick = (() => {
 });
 
 
-<<<<<<< HEAD
 
-=======
->>>>>>> eventos_rama
