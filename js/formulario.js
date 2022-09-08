74a62faeb8d59e45b56bcaad3ccc36d16304e9f7
sofivasconi <sofivasconi@gmail.com>
2022-09-08 13:41:34 -0300

    entrega final

--- js/formulario.js
+++ js/formulario.js
@@ -1,24 +1,22 @@
-
 //variables 
 const formulario = document.querySelector("#form");
+const inputs = document.querySelectorAll("input");
 const email = document.querySelector("#email");
 const nombre = document.querySelector("#firstname");
+const apellido = document.querySelector("#lastname");
 const dni = document.querySelector("#dni");
-const telefono = document.querySelector("#phone");
-const direccion = document.querySelector("#street");
-const numero = document.querySelector("#number"); 
-const botonForm = document.querySelector(".botonForm"); 
-const resumenCompra = document.querySelector("#resumenCompra");
+const telefono = document.querySelector("#phone");  
 const botonFinal = document.querySelector ("#botonFinal");
 
 
-
+/*
 //form
 formulario.addEventListener("submit", validarForm);
 
 function validarForm(evento){
-    (email.value=="")||(nombre.value="")||(dni.value="")||(telefono.value="")||(direccion.value="")||(numero.value="") ? evento.preventDefault () : [];
+    (email.value=="")||(nombre.value="")||(dni.value="")||(telefono.value="") ? evento.preventDefault () : []
 };
+
 
 //toastify     
 if (validarForm){
@@ -34,38 +32,99 @@ if (validarForm){
         }).showToast ();
     });
 }
-
-
-
-//resumen del carrito
-productosFinales = JSON.parse(localStorage.getItem("carrito"));
-console.log(productosFinales);
-
-botonFinal.onclick=async()=> {
-
-    const item =JSON.stringify(productosFinales);
-    let listaResumen = " ";
-
-    item.forEach(item => {
-        listaResumen+= `
-            <img src="${item.imagen}" alt="${item.titulo}" class="cart-img">
-            <div class="cart-product-title">
-                ${item.titulo}
-            </div>
-            <div class="cart-price">
-                $ ${item.precio}
-            </div>
-            <div class="cantidadFinal">
-                <h4>TOTAL: $${total}<h4>
-            </div>
-        `;
-    });
-};
-resumenCompra.innerHTML=listaResumen;
-
-
-/*
-productosFinales.forEach((item) => {
-    resumenCompra.innerHTML += JSON.stringify(productosFinales);
-}); 
 */
+
+
+//validar formulario
+const validarFormulario = () => {
+    formulario.noValidate = true;
+    formulario.addEventListener("submit", e => {
+        e.preventDefault ();
+        inputs.forEach(input => {
+            checkInputs(input);
+        });
+        if (formulario.checkValidar()){
+            Toastify({
+                text: 'Tu compra fue realizada con éxito! Nos vamos a comunicar con vos a través del correo electrónico.',
+                duration: 3000,
+                position: 'center',
+                gravity: 'bottom',
+                style: {
+                    background: '#000000'
+                }
+            }).showToast ();
+        }
+    });
+    inputs.forEach(input => {
+        input.addEventListener("blur", () => {
+            checkInputs(input);
+        });
+    });
+}
+
+const checkInputs = input => {
+    //variables
+    const nombreValue = nombre.value.trim();
+    const apellidoValue = apellido.value.trim();
+    const dniValue = dni.value.trim();
+    const emailValue = email.value.trim();
+    const telefonoValue = telefono.value.trim();
+
+    //patrones
+    const patronLetras = /^[A-ZÀ-Ú]+$/i;
+    const patronNumeros = /^[0-9]+$/;
+
+
+    switch (input) {
+        case nombre:
+            if (nombreValue === "") {
+                setErrorFor(nombre, "Este campo es obligatorio.");
+            } else if (!patronLetras.test(nombreValue)) {
+                setErrorFor(nombre, "Introduzca un nombre válido.");
+            } else {
+                setSuccessFor(nombre);
+            }
+            break;
+
+        case apellido:
+            if (apellidoValue === "") {
+            setErrorFor(apellido, "Este campo es obligatorio.");
+            } else if (!patronLetras.test(apellidoValue)) {
+                setErrorFor(apellido, "Introduzca un apellido válido.");
+            } else {
+                setSuccessFor(apellido);
+            }
+            break;
+    
+        case dni:
+            if (dniValue === "") {
+                setErrorFor(dni, "Este campo es obligatorio.");
+            } else if ((!patronNumeros.test(dniValue)) || (dniValue.length < 6)) {
+                    setErrorFor(dni, "Introduzca un documento válido.");
+            } else {
+                setSuccessFor(dni);
+            }
+            break;
+
+        case email:
+            if (emailValue === "") {
+                setErrorFor(email, "Este campo es obligatorio.");
+            } else if (!isEmail(emailValue)) {
+                setErrorFor(email, "Introduzca un email válido.");
+            } else {
+                setSuccessFor(email);
+            }
+            break;
+
+        case telefono:        
+            if (telefonoValue === "") {
+                setErrorForPhone(telefono, "Este campo es obligatorio.");
+            } else if ((!patronNumeros.test(telefono)) || (telefonoValue.length < 8)) {
+                setErrorForPhone(telefono, "Introduzca un celular válido.");
+            } else {
+                const formControl = telefono.parentElement.parentElement;                    
+                formControl.className = "form-control success";
+            }
+            break;
+    }
+}
