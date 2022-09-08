cb35b5f36634b1c251630ef05f8cfc964394d991
sofivasconi <sofivasconi@gmail.com>
2022-09-08 20:20:11 -0300

    entrega final

--- js/formulario.js
+++ js/formulario.js
@@ -1,12 +1,51 @@
 //variables 
 const formulario = document.querySelector("#form");
-const inputs = document.querySelectorAll("input");
+const inputs = document.querySelectorAll(".input");
 const email = document.querySelector("#email");
-const nombre = document.querySelector("#firstname");
-const apellido = document.querySelector("#lastname");
+const nombre = document.querySelector("#name");
 const dni = document.querySelector("#dni");
 const telefono = document.querySelector("#phone");  
 const botonFinal = document.querySelector ("#botonFinal");
+
+window.addEventListener("load", () => {
+    formulario.addEventListener("submit", (e) => {
+        e.preventDefault();
+        validarCampos()
+    })
+
+    const validarCampos = ()=> {
+        const nombreValor= nombre.value.trim()
+        const dniValor= dni.value.trim()
+        const emailValor= email.value.trim()
+        const telefonoValor= telefono.value.trim();
+
+
+
+        if(nombreValor==="" || dniValor==="" || emailValor==="" ||telefonoValor===""){
+            Toastify({
+                text: 'ERROR, debe llenar todos los campos.',
+                duration: 3000,
+                position: 'center',
+                gravity: 'bottom',
+                style: {
+                    background: '#000000'
+                }
+            }).showToast ();
+        }else{
+            Toastify({
+                text: 'Tu compra ha sido realizada con éxito! Nos vamos a comunicar con vos vía email.',
+                duration: 4000,
+                position: 'center',
+                gravity: 'bottom',
+                style: {
+                    background: '#000000'
+                }
+            }).showToast ();
+        }
+    }
+})
+
+
 
 
 /*
@@ -32,99 +71,85 @@ if (validarForm){
         }).showToast ();
     });
 }
-*/
 
 
-//validar formulario
-const validarFormulario = () => {
-    formulario.noValidate = true;
-    formulario.addEventListener("submit", e => {
-        e.preventDefault ();
-        inputs.forEach(input => {
-            checkInputs(input);
-        });
-        if (formulario.checkValidar()){
+//expresiones
+const expresiones = {
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,//letras y espacios
+	dni: /^\d{8}$/,//numeros
+	email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,//formato email
+	telefono: /^\d{7,14}$/ //numeros
+}
+
+const campos = {
+    nombre: false,
+    dni: false,
+    email: false,
+    telefono: false
+}
+    
+
+//validar form
+const validarFormulario = (e) => {
+    switch (e.target.name) {
+        case "name":
+            if(expresiones.nombre.test(e.target.value)){
+                campos["nombre"]=true;
+            }else{
+                campos["nombre"]=false;
+            }
+        break;
+
+        case "dni":
+            if(expresiones.dni.test(e.target.value)){
+                campos["dni"]=true;
+            }else{
+                campos["dni"]=false;
+            }
+        break;
+
+        case "email":
+            if(expresiones.email.test(e.target.value)){
+                campos["email"] =true;
+            }else{
+                campos["email"]=false;
+            }
+        break;
+
+        case "phone":
+            if(expresiones.telefono.test(e.target.value)){
+                campos["telefono"] =true;
+            }else{
+                campos["telefono"]=false;
+            }
+        break;
+    }
+}
+
+
+//validar datos
+inputs.forEach((input) => {
+    input.addEventListener('keyup', validarFormulario);
+    input.addEventListener('blur', validarFormulario);
+})
+
+
+
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if(campos.nombre && campos.dni && campos.email && campos.telefono){
+        botonFinal.addEventListener('click', () => {
             Toastify({
-                text: 'Tu compra fue realizada con éxito! Nos vamos a comunicar con vos a través del correo electrónico.',
-                duration: 3000,
+                text: 'ok',
+                duration: 1000,
                 position: 'center',
                 gravity: 'bottom',
                 style: {
                     background: '#000000'
                 }
             }).showToast ();
-        }
-    });
-    inputs.forEach(input => {
-        input.addEventListener("blur", () => {
-            checkInputs(input);
         });
-    });
-}
-
-const checkInputs = input => {
-    //variables
-    const nombreValue = nombre.value.trim();
-    const apellidoValue = apellido.value.trim();
-    const dniValue = dni.value.trim();
-    const emailValue = email.value.trim();
-    const telefonoValue = telefono.value.trim();
-
-    //patrones
-    const patronLetras = /^[A-ZÀ-Ú]+$/i;
-    const patronNumeros = /^[0-9]+$/;
-
-
-    switch (input) {
-        case nombre:
-            if (nombreValue === "") {
-                setErrorFor(nombre, "Este campo es obligatorio.");
-            } else if (!patronLetras.test(nombreValue)) {
-                setErrorFor(nombre, "Introduzca un nombre válido.");
-            } else {
-                setSuccessFor(nombre);
-            }
-            break;
-
-        case apellido:
-            if (apellidoValue === "") {
-            setErrorFor(apellido, "Este campo es obligatorio.");
-            } else if (!patronLetras.test(apellidoValue)) {
-                setErrorFor(apellido, "Introduzca un apellido válido.");
-            } else {
-                setSuccessFor(apellido);
-            }
-            break;
-    
-        case dni:
-            if (dniValue === "") {
-                setErrorFor(dni, "Este campo es obligatorio.");
-            } else if ((!patronNumeros.test(dniValue)) || (dniValue.length < 6)) {
-                    setErrorFor(dni, "Introduzca un documento válido.");
-            } else {
-                setSuccessFor(dni);
-            }
-            break;
-
-        case email:
-            if (emailValue === "") {
-                setErrorFor(email, "Este campo es obligatorio.");
-            } else if (!isEmail(emailValue)) {
-                setErrorFor(email, "Introduzca un email válido.");
-            } else {
-                setSuccessFor(email);
-            }
-            break;
-
-        case telefono:        
-            if (telefonoValue === "") {
-                setErrorForPhone(telefono, "Este campo es obligatorio.");
-            } else if ((!patronNumeros.test(telefono)) || (telefonoValue.length < 8)) {
-                setErrorForPhone(telefono, "Introduzca un celular válido.");
-            } else {
-                const formControl = telefono.parentElement.parentElement;                    
-                formControl.className = "form-control success";
-            }
-            break;
     }
-}
+});
+*/
